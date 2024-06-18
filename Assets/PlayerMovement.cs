d95381b75495b0fbdfd23078cc2f576987bb1e7d
DeltDev <deltenc88@gmail.com>
2024-06-18 22:39:52 +0700

    feat: first person and third person movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,10 +8,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float swimSpeed;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float rotationSmoothTime = 0.1f;
+    [SerializeField] private Transform cameraTransform;
     private float rotationSmoothVelocity;
     private Rigidbody rb;
     private void Awake() {
         rb = GetComponent<Rigidbody>();
+        HideAndLockCursor();
+    }
+
+    private void HideAndLockCursor(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     private void Start() {
         inputManager.OnMoveInput += Move;
@@ -23,12 +30,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move(Vector2 axisDirection){
         if(axisDirection.magnitude >= 0.1){
-            float rotationAngle = Mathf.Atan2(axisDirection.x, axisDirection.y) * Mathf.Rad2Deg;
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y,rotationAngle,ref rotationSmoothVelocity, rotationSmoothTime);
-            transform.rotation = Quaternion.Euler(0f,smoothAngle,90f);
-            Vector3 movementDirection = Quaternion.Euler(0f,rotationAngle,0f) * Vector3.forward;
+            float clampedY = cameraTransform.eulerAngles.y;
+            clampedY = Mathf.Clamp(clampedY,-60f,60f);
+            float rotationAngle1 = Mathf.Atan2(axisDirection.x, axisDirection.y) * Mathf.Rad2Deg + clampedY;
+            float smoothAngle1 = Mathf.SmoothDampAngle(transform.eulerAngles.y,rotationAngle1,ref rotationSmoothVelocity, rotationSmoothTime);
+            float cameraPitch = cameraTransform.eulerAngles.x;
+            if (cameraPitch > 180){
+                cameraPitch -= 360;
+            } 
+            cameraPitch = Mathf.Clamp(cameraPitch, -60f, 60f);
+            Quaternion targetRotation = Quaternion.Euler(90f + cameraPitch, smoothAngle1, 0f);
+            rb.MoveRotation(targetRotation);
+            Vector3 movementDirection = Quaternion.Euler(cameraPitch,rotationAngle1,0f) * Vector3.forward;
             Debug.Log(movementDirection);
             rb.AddForce(swimSpeed * Time.deltaTime * movementDirection);
+        } else {
+            rb.velocity = Vector3.zero;
         }
     }
 }
